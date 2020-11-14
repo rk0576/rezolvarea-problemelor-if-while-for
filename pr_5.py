1b7919395d41d48d6b29c022fc9093f5c6bc837f
rk0576 <70932901+rk0576@users.noreply.github.com>
2020-11-14 22:36:34 +0200

    Add files via upload

---
+++ pr_5.py
@@ -0,0 +1,25 @@
+a=int(input('Introduceti anul: '))
+if a%12==8:
+    print('dragon')
+if a%12==7:
+    print('iepure')
+if a%12==6:
+    print('tigru')
+if a%12==5:
+    print('bou')
+if a%12==4:
+    print('sobolan')
+if a%12==9:
+    print('sarpe')
+if a%12==10:
+    print('cal')
+if a%12==11:
+    print('oaie')
+if a%12==0:
+    print('maimuta')
+if a%12==3:
+    print('porc')
+if a%12==2:
+    print('caine')
+if a%12==1:
+    print('cocos')
